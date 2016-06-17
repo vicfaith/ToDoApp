c042fc7325d9e96bc88619ddcb1fac4025316393
dkang_wow <dkang@woolworths.com.au>
2016-06-17 11:45:07 +1000

    add initial web app - cron does not work

--- backend/src/main/java/com/example/dkang/myapplication/backend/MyServlet.java
+++ backend/src/main/java/com/example/dkang/myapplication/backend/MyServlet.java
@@ -6,25 +6,71 @@
 
 package com.example.dkang.myapplication.backend;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class MyServlet extends HttpServlet {
+    static Logger Log = Logger.getLogger("com.example.dkang.myapplication.backend.MyServlet");
+
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().println("Please use the form to POST to this url");
-    }
+        Log.info("Got cron message, constructing email.");
 
-    @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-        String name = req.getParameter("name");
-        resp.setContentType("text/plain");
-        if(name == null) {
-            resp.getWriter().println("Please enter a name");
-        }
-        resp.getWriter().println("Hello " + name);
+        //Create a new Firebase instance and subscribe on child events.
+        Firebase firebase = new Firebase("https://my-first-app-4597d.firebaseio.com/todoItems");
+        firebase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Build the email message contents using every field from Firebase.
+                final StringBuilder newItemMessage = new StringBuilder();
+                newItemMessage.append("Good Morning!  You have the following todo items:\n");
+                for (DataSnapshot todoItem : dataSnapshot.getChildren()) {
+                    for (DataSnapshot field : todoItem.getChildren()) {
+                        newItemMessage.append(field.getKey())
+                                .append(":")
+                                .append(field.getValue().toString())
+                                .append("\n");
+                    }
+                }
+
+                //Now Send the email
+                Properties props = new Properties();
+                Session session = Session.getDefaultInstance(props, null);
+                try {
+                    Message msg = new MimeMessage(session);
+                    //Make sure you substitute your project-id in the email From field
+                    msg.setFrom(new InternetAddress("nagger@my-first-app-4597d.appspotmail.com",
+                            "Todo Nagger"));
+                    msg.addRecipient(Message.RecipientType.TO,
+                            new InternetAddress("vicfaith2010@gmail.com", "Recipient"));
+                    msg.setSubject("Good Morning!");
+                    msg.setText(newItemMessage.toString());
+                    Transport.send(msg);
+                } catch (MessagingException | UnsupportedEncodingException e) {
+                    Log.warning(e.getMessage());
+                }
+            }
+
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
     }
 }
